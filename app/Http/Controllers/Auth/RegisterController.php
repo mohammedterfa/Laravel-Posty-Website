5cb66059d698275b98c203a147f2658a2618c3a5
mohammedterfa <mohammedterfa@gmail.com>
2022-05-15 14:11:09 +0200

    validate user inputs in register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,16 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(){
-        dd('123');
+    public function store(Request $request){
+        // validation
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+        // store user
+        // sign the user in
+        // redirect
     }
 }
