8c02f32ad7e0bbd1f3b0b19727d9721d3387a11c
mohammedterfa <mohammedterfa@gmail.com>
2022-05-15 20:08:05 +0200

    making navbar better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="{{ url('posts') }}" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Post</a>
