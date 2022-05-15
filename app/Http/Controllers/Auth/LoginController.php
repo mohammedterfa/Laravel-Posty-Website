37e7fcab9eb8c4d491f82cb3d6ee3233a9ddb463
mohammedterfa <mohammedterfa@gmail.com>
2022-05-15 15:51:08 +0200

    login view was made:

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function index(){
+        return view('auth.login');
+    }
+
+    public function store(){
 
     }
 }
