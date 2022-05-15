9f43d1957f69fd5d7be1c302a7bea13e984210ad
mohammedterfa <mohammedterfa@gmail.com>
2022-05-15 17:52:34 +0200

    login controller was now working and logout was made

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,7 +11,16 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function store(){
+    public function store(Request $request){
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
+        if(!auth()->attempt($request->only('email', 'password'))){
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('dashboard');
     }
 }
