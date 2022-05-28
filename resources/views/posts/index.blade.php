76a00c06f8563787a938902f78296b36b0304138
mohammedterfa <mohammedterfa@gmail.com>
2022-05-28 16:06:07 +0200

    post buttom created

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,6 +19,9 @@
 
                 </div>
 
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                </div>
             </form>
         </div>
     </div>
