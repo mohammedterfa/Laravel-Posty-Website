c74c9f820eb7a674487cde48bdbce64d8745b625
mohammedterfa <mohammedterfa@gmail.com>
2022-05-28 17:03:43 +0200

    store post was now working

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -11,6 +12,14 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
 
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
     }
 }
