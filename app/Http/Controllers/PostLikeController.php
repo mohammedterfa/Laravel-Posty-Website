fbb2f08279482d9f743c4794ca1e6adfb1e14790
mohammedterfa <mohammedterfa@gmail.com>
2022-06-10 19:13:09 +0200

    code

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -8,6 +8,12 @@ use App\Models\Post;
 class PostLikeController extends Controller
 {
     public function store(Post $post, Request $request){
+
+        if($post->likedBy($request->user())){
+            return response(null, 409);
+        }
+
+
         $post->likes()->create([
             'user_id' => $request->user()->id,
         ]);
