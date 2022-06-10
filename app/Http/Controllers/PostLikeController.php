9c94a648026b4ff36849869b67ac2e6c9b95c2a9
mohammedterfa <mohammedterfa@gmail.com>
2022-06-10 19:03:27 +0200

    likes counter works!!

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -7,7 +7,11 @@ use App\Models\Post;
 
 class PostLikeController extends Controller
 {
-    public function store(Post $post){
+    public function store(Post $post, Request $request){
+        $post->likes()->create([
+            'user_id' => $request->user()->id,
+        ]);
 
+        return back();
     }
 }
