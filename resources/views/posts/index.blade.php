b6bad1e8cff81eca05e83cd73619a230c310cbc4
mohammedterfa <mohammedterfa@gmail.com>
2022-06-11 22:35:59 +0200

    add some code

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,11 +30,17 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
 
+                        <div>
+                            <form action="" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Delete</button>
+                            </form>
+                        </div>
+
                         <div class="flex items-center">
                             @auth
                                 @if (!$post->likedBy(auth()->user()))
-
-
                                 <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
                                     @csrf
                                     <button type="submit" class="text-blue-500">Like</button>
@@ -48,6 +54,8 @@
                                     <button type="submit" class="text-blue-500">Unlink</button>
                                 </form>
                                 @endif
+
+
                             @endauth
 
                             <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
